94223ac53eeae2830c72300162bb3d0909a46cbc
Encore Chow <encorechow1992@gmail.com>
2016-08-18 11:11:38 -0700

    Added unittest file and completed ColorDescriptor implementation

--- colordescriptor.py
+++ colordescriptor.py
@@ -45,3 +45,13 @@ class ColorDescriptor(object):
         features.extend(hist)
 
         return features
+
+
+    def histogram(self, image, mask):
+        # Extract the histogram from HSV color image with its masked region.
+        # Using provided bins per channel; then normalize the histogram.
+
+        hist = cv2.calcHist([image], [0, 1, 2], mask, self.bins, [0, 180, 0, 256, 0, 256])
+        hist = cv2.normalize(hist).flatten()
+
+        return hist
